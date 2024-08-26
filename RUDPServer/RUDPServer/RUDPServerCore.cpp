2725cbbd3c3a011f6bb8fec52b8f6006b1fb91fa
m5623skhj <m5623skhj@gmail.com>
2024-08-27 01:05:21 +0900

    * IO 스레드 추가 ** 스레드 로직 구현은 아직임 * RIO 초기화 함수 추가

--- RUDPServer/RUDPServer/RUDPServerCore.cpp
+++ RUDPServer/RUDPServer/RUDPServerCore.cpp
@@ -12,6 +12,12 @@ RUDPServerCore::RUDPServerCore()
 
 bool RUDPServerCore::StartServer(const std::wstring_view& optionFilePath)
 {
+	if (InitializeRIO() == false)
+	{
+		std::cout << "InitializeRIO failed" << std::endl;
+		return false;
+	}
+
 	if (InitNetwork() == false)
 	{
 		std::cout << "InitNetwork failed" << std::endl;
@@ -26,6 +32,16 @@ void RUDPServerCore::StopServer()
 {
 	closesocket(sock);
 	WSACleanup();
+
+	for (auto& thread : ioThreadList)
+	{
+		thread.join();
+	}
+
+	for (auto& rioCQ : rioCQList)
+	{
+		rioFunctionTable.RIOCloseCompletionQueue(*rioCQ);
+	}
 
 	std::cout << "Server stop" << std::endl;
 }
@@ -62,6 +78,34 @@ bool RUDPServerCore::InitNetwork()
 		closesocket(sock);
 		WSACleanup();
 		return false;
+	}
+
+	return true;
+}
+
+bool RUDPServerCore::InitializeRIO()
+{
+	GUID functionTableId = WSAID_MULTIPLE_RIO;
+	DWORD bytes = 0;
+
+	if (WSAIoctl(sock
+		, SIO_GET_MULTIPLE_EXTENSION_FUNCTION_POINTER
+		, &functionTableId
+		, sizeof(GUID)
+		, reinterpret_cast<void**>(&rioFunctionTable)
+		, sizeof(rioFunctionTable)
+		, &bytes
+		, NULL
+		, NULL))
+	{
+		std::cout << "InitializeRIO failed " << WSAGetLastError() << std::endl;
+		return false;
+	}
+
+	rioCQList.reserve(threadCount);
+	for (int i = 0; i < threadCount; ++i)
+	{
+		rioCQList.push_back(new RIO_CQ());
 	}
 
 	return true;
--- RUDPServer/RUDPServer/RUDPServerCore.h
+++ RUDPServer/RUDPServer/RUDPServerCore.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <string_view>
+#include <thread>
+#include <vector>
+#include <MSWSock.h>
 
 class RUDPServerCore
 {
@@ -24,4 +27,19 @@ private:
 private:
 	SOCKET sock;
 	unsigned short port;
+
+#pragma region threads
+private:
+	std::vector<std::thread> ioThreadList;
+	std::vector<RIO_CQ*> rioCQList;
+	unsigned short threadCount;
+#pragma endregion threads
+
+#pragma region RIO
+private:
+	bool InitializeRIO();
+
+private:
+	RIO_EXTENSION_FUNCTION_TABLE rioFunctionTable;
+#pragma endregion RIO
 };
